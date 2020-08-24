b5a98ff6be42053d22bd050b4b8a376699e3d63a
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-24 11:55:08 -0400

    Brew formula update for bridge-vpn-cli version v0.1.0-rc6

--- Formula/bridge-vpn-cli.rb
+++ Formula/bridge-vpn-cli.rb
@@ -3,16 +3,16 @@ require_relative "../download-strategy.rb"
 class BridgeVpnCli < Formula
   desc ""
   homepage ""
-  version "0.1.0-rc5"
+  version "0.1.0-rc6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/instructure-bridge/bridge-vpn-cli/releases/download/v0.1.0-rc5/bridge-vpn-cli_0.1.0-rc5_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "bed34b9a86003bdecbbfa9d95a9d2c18cd5ba4e094fd419ed394fe82f5cefdcb"
+    url "https://github.com/instructure-bridge/bridge-vpn-cli/releases/download/v0.1.0-rc6/bridge-vpn-cli_0.1.0-rc6_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "bf006f9ef3cb00ecff7bef202db5abb822013370482f48b1cc26cc39e96c92fe"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/instructure-bridge/bridge-vpn-cli/releases/download/v0.1.0-rc5/bridge-vpn-cli_0.1.0-rc5_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "329994bc65177cf5aa6e936cc4a2609db4309eea0885a25dfee7dac837503100"
+      url "https://github.com/instructure-bridge/bridge-vpn-cli/releases/download/v0.1.0-rc6/bridge-vpn-cli_0.1.0-rc6_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "3af7b65cecd1734f2373d11494f0a3d4d2406399dcf391b3701c2dbb62b17642"
     end
   end
   
@@ -20,11 +20,11 @@ class BridgeVpnCli < Formula
   depends_on "openconnect"
 
   def install
-    system "cp", "bridge-vpn-cli", "bvpn"
-    bin.install "bvpn"
+    system "cp", "bridge-vpn-cli", "bridge-vpn"
+    bin.install "bridge-vpn"
   end
 
   test do
-    system bin/"bvpn", "help"
+    system bin/"bridge-vpn", "help"
   end
 end
