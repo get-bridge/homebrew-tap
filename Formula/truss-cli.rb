5a60e84a7eecc4413d8ac0feb9458ae3e11b904f
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-03 13:19:56 -0500

    Brew formula update for truss-cli version v0.2.4

--- Formula/truss-cli.rb
+++ Formula/truss-cli.rb
@@ -5,29 +5,41 @@
 class TrussCli < Formula
   desc "CLI to help manage many k8s clusters"
   homepage "https://github.com/get-bridge/truss-cli"
-  version "0.2.3"
+  version "0.2.4"
 
-  if OS.mac?
-    url "https://github.com/get-bridge/truss-cli/releases/download/v0.2.3/truss-cli_0.2.3_Darwin_x86_64.tar.gz"
-    sha256 "b99a8d43c4f0ed47887e6333f6423dcc83d49283fa20983710c21c623fce5ba1"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/get-bridge/truss-cli/releases/download/v0.2.4/truss-cli_0.2.4_Darwin_x86_64.tar.gz"
+      sha256 "ae654dfa3742941a62d5e23f689b15855b4368ea562aeef8603040918561d234"
+
+      def install
+        bin.install "truss"
+      end
+    end
   end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/get-bridge/truss-cli/releases/download/v0.2.3/truss-cli_0.2.3_Linux_x86_64.tar.gz"
-    sha256 "e885d4a69f3b2138bc8eb4c8b7b3c6303362f27a5f79cfc5580c6de781729e32"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/get-bridge/truss-cli/releases/download/v0.2.3/truss-cli_0.2.3_Linux_arm64.tar.gz"
-    sha256 "7ea6f8161346ec0fb207faadbff69c010679c5f84154895baf637da4cb95eb78"
+
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/get-bridge/truss-cli/releases/download/v0.2.4/truss-cli_0.2.4_Linux_arm64.tar.gz"
+      sha256 "d8520769ee3f225c78422998e3d19dbd7af9c457433295fbf16ee5f63aeeb232"
+
+      def install
+        bin.install "truss"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/get-bridge/truss-cli/releases/download/v0.2.4/truss-cli_0.2.4_Linux_x86_64.tar.gz"
+      sha256 "3cca685d9804708c97d6726b936be38cb2d6241d893d140e4f70480e58ff6f40"
+
+      def install
+        bin.install "truss"
+      end
+    end
   end
 
   depends_on "kubectl"
   depends_on "vault"
   depends_on "sshuttle"
-
-  def install
-    system "cp", "truss-cli", "truss"
-    bin.install "truss"
-  end
 
   test do
     system "bin/truss", "help"
